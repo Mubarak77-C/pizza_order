d99ebd51c0d9756842384f347667d1cacc44ddce
Mubarak <mubaraksache77@gmail.com>
2020-11-13 02:47:24 +0500

    Updates

---
+++ precache-manifest.fada5ab5488638c231ff612fa708acee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecd1d2572eb80743ffbca3cd5bb6506b",
+    "url": "/pizza_order/index.html"
+  },
+  {
+    "revision": "74637ed3e9f9476a8ac6",
+    "url": "/pizza_order/static/css/main.d0434e9a.chunk.css"
+  },
+  {
+    "revision": "79aff2c418ae31b888a8",
+    "url": "/pizza_order/static/js/2.f6132584.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pizza_order/static/js/2.f6132584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74637ed3e9f9476a8ac6",
+    "url": "/pizza_order/static/js/main.41f81646.chunk.js"
+  },
+  {
+    "revision": "f24392203e7c5b60ddec",
+    "url": "/pizza_order/static/js/runtime-main.761fc9a5.js"
+  }
+]);
